728229708cacecd78dc83f0c952e7b9b58648c89
Łukasz Kalbarczyk <l.kalbarczyk@gmail.com>
2024-09-23 14:17:49 +0200

    feat: docs&version updated

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 //! # RedStone
 //!
 //! `redstone` is a collection of utilities to make deserializing&decrypting RedStone payload.
-//! It contains a pure Rust implementation and also an extension for the Casper network.
+//! It includes a pure Rust implementation, along with extensions for certain networks.
 //!
 //! Different crypto-mechanisms are easily injectable.
 //! The current implementation contains `secp256k1`- and `k256`-based variants.
