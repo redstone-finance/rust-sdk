ac334d58128c039428ecac504d7a03e7d9e268e1
Bartossh <lenart.consulting@gmail.com>
2025-01-08 17:46:12 +0100

    Rename benchmark function.

--- crates/redstone/benches/benchmarks.rs
+++ crates/redstone/benches/benchmarks.rs
@@ -3,13 +3,13 @@ use redstone::helpers::slice::has_duplicates;
 
 const ELEM_COUNT: usize = 256;
 
-fn benchmark_has_repetition(c: &mut Criterion) {
+fn benchmark_has_duplicates(c: &mut Criterion) {
     let mut nums = vec![];
     for i in (0..ELEM_COUNT).rev() {
         nums.push(i);
     }
 
-    c.bench_function("benchmark_has_repetition", |b| {
+    c.bench_function(" benchmark_has_duplicates", |b| {
         b.iter(|| {
             if let Some(_) = has_duplicates(&nums) {
                 panic!("Shouldn't find any repetition in benchmark");
@@ -18,6 +18,6 @@ fn benchmark_has_repetition(c: &mut Criterion) {
     });
 }
 
-criterion_group!(benches, benchmark_has_repetition,);
+criterion_group!(benches, benchmark_has_duplicates,);
 
 criterion_main!(benches);
