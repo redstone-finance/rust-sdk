d9a4d0f931a258dd50e3751a4fffba6f3dddd510
Jan Koscisz <koscis.j@gmail.com>
2025-04-16 08:23:17 +0200

    tstmp

--- crates/redstone_testing/src/sample/mod.rs
+++ crates/redstone_testing/src/sample/mod.rs
@@ -110,7 +110,7 @@ pub fn sample_eth_3sig() -> Sample {
             "ETH" => 159504422175_u128
         ],
         timestamp: 1744563500000,
-        system_timestamp: 0,
+        system_timestamp: 1744563500000,
         signers: Signers::Primary,
     }
 }
