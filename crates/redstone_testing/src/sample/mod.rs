34944dc44d1d297055b4c783abec3e6cd2bc36c7
Jan Koscisz <koscis.j@gmail.com>
2025-04-17 16:28:13 +0200

    0x

--- crates/redstone_testing/src/sample/mod.rs
+++ crates/redstone_testing/src/sample/mod.rs
@@ -32,19 +32,19 @@ impl Signers {
 }
 
 pub const AVAX_SIGNERS: [&str; 5] = [
-    "0x109B4a318A4F5ddcbCA6349B45f881B4137deaFB",
-    "0x12470f7aba85c8b81d63137dd5925d6ee114952b",
-    "0x1ea62d73edf8ac05dfcea1a34b9796e937a29eff",
-    "0x83cba8c619fb629b81a65c2e67fe15cf3e3c9747",
-    "0x2c59617248994D12816EE1Fa77CE0a64eEB456BF",
+    "109B4a318A4F5ddcbCA6349B45f881B4137deaFB",
+    "12470f7aba85c8b81d63137dd5925d6ee114952b",
+    "1ea62d73edf8ac05dfcea1a34b9796e937a29eff",
+    "83cba8c619fb629b81a65c2e67fe15cf3e3c9747",
+    "2c59617248994D12816EE1Fa77CE0a64eEB456BF",
 ];
 
 pub const PRIMARY_SIGNERS: [&str; 5] = [
-    "0x8bb8f32df04c8b654987daaed53d6b6091e3b774",
-    "0xdeb22f54738d54976c4c0fe5ce6d408e40d88499",
-    "0x51ce04be4b3e32572c4ec9135221d0691ba7d202",
-    "0xdd682daec5a90dd295d14da4b0bec9281017b5be",
-    "0x9c5ae89c4af6aa32ce58588dbaf90d18a855b6de",
+    "8bb8f32df04c8b654987daaed53d6b6091e3b774",
+    "deb22f54738d54976c4c0fe5ce6d408e40d88499",
+    "51ce04be4b3e32572c4ec9135221d0691ba7d202",
+    "dd682daec5a90dd295d14da4b0bec9281017b5be",
+    "9c5ae89c4af6aa32ce58588dbaf90d18a855b6de",
 ];
 
 #[derive(Debug, Clone)]
